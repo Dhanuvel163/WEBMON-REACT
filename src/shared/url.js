e535d0bb3b91abd35726f70cd18473a373023626
Dhanuvel163 <dhanuram99@gmail.com>
2021-02-25 10:59:55 +0530

    PROD SETUP

--- src/shared/url.js
+++ src/shared/url.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV=='DEV' ?"http://localhost:3030/":"https://web-mon.herokuapp.com/";
+export const baseUrl = process.env.NODE_ENV=="development" ?"http://localhost:3030/":"https://web-mon.herokuapp.com/";
